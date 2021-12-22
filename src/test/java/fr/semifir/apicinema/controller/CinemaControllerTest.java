4246fb0ae0728ed33a6a5af7dca150f99b6b7c5b
clement <clement.cauliez@gmail.com>
2021-12-22 15:41:25 +0100

    test: [cinema] added update tests

--- src/test/java/fr/semifir/apicinema/controller/CinemaControllerTest.java
+++ src/test/java/fr/semifir/apicinema/controller/CinemaControllerTest.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import fr.semifir.apicinema.controllers.CinemaController;
 import fr.semifir.apicinema.dtos.cinema.CinemaDTO;
+import fr.semifir.apicinema.entities.Cinema;
 import fr.semifir.apicinema.services.CinemaService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,8 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,7 +35,9 @@ public class CinemaControllerTest {
     public CinemaDTO cinemaDTO(){
         return new CinemaDTO("azmdkamzkfa", "gaumont");
     }
-
+    public CinemaDTO cinemaPatheDTO(){
+        return new CinemaDTO("azmdkamzkfa", "Pathe");
+    }
     public Gson gson(){
         return new GsonBuilder().create();
     }
@@ -65,7 +68,7 @@ public class CinemaControllerTest {
 
     @Test
     public void testFindOneCinema() throws Exception{
-        // create a cinema to find and persists it
+        // create a cinema to find and pretend it's persisted
         BDDMockito.given(cinemaService.findByID("azmdkamzkfa"))
                 .willReturn(Optional.of(cinemaDTO()));
         // verify something was created and stores the result of the request
@@ -77,5 +80,43 @@ public class CinemaControllerTest {
         CinemaDTO cinemaDTO = gson().fromJson(result.getResponse().getContentAsString(),
                 CinemaDTO.class);
         Assertions.assertEquals(cinemaDTO.getNom(), "gaumont");
+    }
+
+    @Test
+    public void testUpdateCinema() throws Exception{
+        // create a cinema to update later
+        BDDMockito.given(cinemaService.findByID("azmdkamzkfa"))
+                .willReturn(Optional.of(cinemaDTO()));
+        // store the created entity as a query result
+        MvcResult result = this.mockMvc.perform(get("/cinemas/azmdkamzkfa"))
+                .andExpect(status().isOk())
+                .andReturn();
+        CinemaDTO cinemaDTO = gson().fromJson(result.getResponse().getContentAsString(),
+                CinemaDTO.class);
+        // change the value of an attribute to verify the update works as intended
+        cinemaDTO.setNom("Pathe");
+        // map the dto to a json to update it inside the mock db
+        String json = gson().toJson(cinemaDTO);
+        // here we tell the test that whenever it'll ask for a save with a cinemaDTO object
+        // it will return the result of cinemaPatheDTO =
+        // since we fake the db, for the test to be successful we have to give it
+        // an object that'll be identical to the one we test against (here, names = Pathe)
+        BDDMockito.when(cinemaService.save(any(CinemaDTO.class)))
+                .thenReturn(cinemaPatheDTO());
+        MvcResult mvcResult = this.mockMvc.perform(put("/cinemas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+        // map the result back to a dto to check if the data is consistent
+        CinemaDTO finalCinemaDTO = gson().fromJson(mvcResult.getResponse().getContentAsString(),
+                CinemaDTO.class);
+        Assertions.assertEquals(finalCinemaDTO.getNom(), "Pathe");
+    }
+
+    @Test
+    public void testDeleteCinema(){
+        // have to create a cinema to delete
+
     }
 }
